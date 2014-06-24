0c0fdd8cf0097d03cb47e1cf6e896dcb66bc8693
John Byrd <jbyrd@giganticsoftware.com>
2014-06-24 00:58:58 -0700

    Engine shootout runs against itself

--- tests/engine-shootout.bat
+++ tests/engine-shootout.bat
@@ -1,2 +1,7 @@
 rem engine-shootout
-..\tools\win32\cutechess-cli.exe -debug -engine tc=inf proto=uci name=Ippon cmd=Ippon.exe dir=..\build\vs2012\x86\RelWithDebInfo -engine tc=inf proto=uci name=Ippon cmd=Ippon.exe dir=..\build\vs2012\x86\RelWithDebInfo
+SET varYYYY=%DATE:~10,4%
+SET varMM=%DATE:~4,2%
+SET varDD=%DATE:~7,2%
+SET varTodaysDate=%varYYYY%%varMM%%varDD%
+SET PGN_DATABASE=%varTodaysDate%.pgn
+..\tools\win32\cutechess-cli\cutechess-cli.exe -pgnout %PGN_DATABASE% -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x64\release -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x86\release -each proto=uci tc=60/180 -games 10 -wait 1
