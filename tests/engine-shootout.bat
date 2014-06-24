63f8b8be3d509eeeca6bd0804b990be536b30d1e
John Byrd <jbyrd@giganticsoftware.com>
2014-06-24 01:17:27 -0700

    Engine shootout now runs superpawn against itself

--- tests/engine-shootout.bat
+++ tests/engine-shootout.bat
@@ -1,5 +1,9 @@
 rem engine-shootout
+set TARGETDIR=..\build\tests
+if exist %TARGETDIR%\nul goto setdate
+mkdir %TARGETDIR%
+:setdate
 set mydate=
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
-SET PGN_DATABASE=..\build\tests\engine-test-%mydate%.pgn
+SET PGN_DATABASE=%TARGETDIR%\engine-test-%mydate%.pgn
 ..\tools\win32\cutechess-cli\cutechess-cli.exe -pgnout %PGN_DATABASE% -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x64\release -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x86\release -each proto=uci tc=60/180 -games 10 -wait 1
