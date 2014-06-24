0578266dfb75b499edab2681cb08a2797fdec34b
John Byrd <jbyrd@giganticsoftware.com>
2014-06-24 01:27:52 -0700

    Engine now plays against Testina in tests

--- tests/engine-shootout.bat
+++ tests/engine-shootout.bat
@@ -6,4 +6,4 @@ mkdir %TARGETDIR%
 set mydate=
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
 SET PGN_DATABASE=%TARGETDIR%\engine-test-%mydate%.pgn
-..\tools\win32\cutechess-cli\cutechess-cli.exe -pgnout %PGN_DATABASE% -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x64\release -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x86\release -each proto=uci tc=60/180 -games 10 -wait 1
+..\tools\win32\cutechess-cli\cutechess-cli.exe -pgnout %PGN_DATABASE% -engine name=superpawn proto=uci cmd=superpawn.exe dir=..\build\win\x64\release -engine name=testina proto=xboard cmd=testina.exe dir=engines\testina -each tc=60/180 -games 10 -wait 1
