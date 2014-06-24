9b09757185cda1e02aa782a8e2f303c9629c1972
John Byrd <jbyrd@giganticsoftware.com>
2014-06-24 01:17:02 -0700

    Engine shootout now sends output to build directory.

--- tests/engine-shootout.bat
+++ tests/engine-shootout.bat
@@ -1,7 +1,5 @@
 rem engine-shootout
-SET varYYYY=%DATE:~10,4%
-SET varMM=%DATE:~4,2%
-SET varDD=%DATE:~7,2%
-SET varTodaysDate=%varYYYY%%varMM%%varDD%
-SET PGN_DATABASE=%varTodaysDate%.pgn
+set mydate=
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
+SET PGN_DATABASE=..\build\tests\engine-test-%mydate%.pgn
 ..\tools\win32\cutechess-cli\cutechess-cli.exe -pgnout %PGN_DATABASE% -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x64\release -engine name=superpawn cmd=superpawn.exe dir=..\build\win\x86\release -each proto=uci tc=60/180 -games 10 -wait 1
