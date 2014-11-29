ffd39be8ab995d4a27d9cc9fdccee41b0ec87b44
John Byrd <jbyrd@giganticsoftware.com>
2014-11-29 13:55:54 -0800

    Check in Lua engine rewrite for handling tournaments via cutechess.

--- tests/engine-shootout.bat
+++ tests/engine-shootout.bat
@@ -1,9 +1,4 @@
 rem engine-shootout
-set TARGETDIR=..\build\tests
-if exist %TARGETDIR%\nul goto setdate
-mkdir %TARGETDIR%
-:setdate
-set mydate=
-for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
-SET PGN_DATABASE=%TARGETDIR%\engine-test-%mydate%.pgn
-..\tools\win32\cutechess-cli\cutechess-cli.exe -pgnout %PGN_DATABASE% -engine name=superpawn proto=uci cmd=superpawn.exe dir=..\build\win\x64\release -engine name=testina proto=xboard cmd=testina.exe dir=engines\testina -each tc=60/180 -games 10 -wait 1
+rem Run a tournament of the most recent Superpawn build versus a 
+rem bunch of engines
+..\tools\win32\lua\lua52.exe engine-shootout.lua
