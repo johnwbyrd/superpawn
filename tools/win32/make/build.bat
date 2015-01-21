0b056e6ddd42fffaf7619fd8b6cd88ed5db29b0a
John Byrd <jbyrd@giganticsoftware.com>
2015-01-21 12:21:17 -0800

    Build now reports a BuildInfo.txt file and also labels the binaries better on Windows

--- tools/win32/make/build.bat
+++ tools/win32/make/build.bat
@@ -1,9 +1,9 @@
 echo Run me in the root directory of the project to build win32 releases.
 echo Use -tests argument to run gauntlet tests on engine.
 
-set BUILD_ID=Unknown
+set BUILD_ID=UnknownBuildID
 set BUILD_NUMBER=Unknown
-set BUILD_TAG=Unknown
+set BUILD_TAG=UnknownBuildTag
 
 :parse_command_line
 IF NOT "%1"=="" (
@@ -29,6 +29,7 @@ IF NOT "%1"=="" (
 call "%VS120COMNTOOLS%\vsvars32.bat"
 set ZIP_EXE=tools\win32\zip\zip.exe
 set ENGINE_NAME=superpawn
+set BUILDINFO=release\BuildInfo.txt
 rem
 mkdir build\win\x86
 pushd build\win\x86
@@ -39,7 +40,8 @@ msbuild superpawn.sln /p:Configuration=Debug
 if errorlevel 1 goto :fail
 msbuild superpawn.sln /p:Configuration=Release
 if errorlevel 1 goto :fail
-git log -n 50 > release\ChangeLog.txt
+call :create_ancillary_files
+move release\Superpawn.exe release\Superpawn-%BUILD_NUMBER%-x86.exe
 ..\..\..\%ZIP_EXE% superpawn-windows-x32.zip release\*.*
 popd
 
@@ -51,7 +53,8 @@ msbuild superpawn.sln /p:Configuration=Debug
 if errorlevel 1 goto :fail
 msbuild superpawn.sln /p:Configuration=Release
 if errorlevel 1 goto :fail
-git log -n 50 > release\ChangeLog.txt
+call :create_ancillary_files
+move release\Superpawn.exe release\Superpawn-%BUILD_NUMBER%-x64.exe
 ..\..\..\%ZIP_EXE% superpawn-windows-x64.zip release\*.*
 popd
 
@@ -63,9 +66,22 @@ call gauntlet-test.bat --BUILD_ID %BUILD_ID% --BUILD_NUMBER %BUILD_NUMBER% --BUI
 
 goto done
 
+:create_ancillary_files
+git log -n 50 > release\ChangeLog.txt
+if exist %BUILDINFO% del %BUILDINFO%
+echo This information uniquely identifies this build of Superpawn. >> %BUILDINFO%
+echo Please reference this information when reporting bugs or other issues. >> %BUILDINFO%
+echo --- >> %BUILDINFO%
+echo Build number: %BUILD_NUMBER% >> %BUILDINFO%
+echo Build ID:     %BUILD_ID% >> %BUILDINFO%
+echo Build tag:    %BUILD_TAG% >> %BUILDINFO%
+exit /b
+
 :fail
 echo Build FAILED.
-exit 1
+goto die
 
 :done
 echo Build completed successfully.
+
+:die
