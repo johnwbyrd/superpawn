4d9698837233f941095117ee5a9b16e0ddc0bdbb
John Byrd <jbyrd@giganticsoftware.com>
2015-01-25 00:49:17 -0800

    Build now generates a label at the top of index.html describing the current build.

--- tools/win32/make/build.bat
+++ tools/win32/make/build.bat
@@ -30,7 +30,8 @@ IF NOT "%1"=="" (
 call "%VS120COMNTOOLS%\vsvars32.bat"
 set ZIP_EXE=tools\win32\zip\zip.exe
 set ENGINE_NAME=superpawn
-set BUILDINFO=release\BuildInfo.txt
+set BUILDINFO=BuildInfo.txt
+set CHANGELOG=ChangeLog.txt
 rem Get current branch
 git rev-parse --abbrev-ref HEAD > git-branch.txt
 set /p BUILD_BRANCH=<git-branch.txt
@@ -38,6 +39,7 @@ del git-branch.txt
 rem
 mkdir build\win\x86
 pushd build\win\x86
+call :create_ancillary_files
 rem
 cmake -G "Visual Studio 12" ..\..\..
 rem
@@ -47,20 +49,24 @@ msbuild superpawn.sln /p:Configuration=Release
 if errorlevel 1 goto :fail
 call :create_ancillary_files
 copy release\Superpawn.exe release\Superpawn-%BUILD_NUMBER%-x86.exe
+copy %BUILDINFO% release /y
+copy %CHANGELOG% release /y
 cd release
 ..\..\..\..\%ZIP_EXE% ..\superpawn-windows-x32.zip *.* -x superpawn.exe
 popd
 
 mkdir build\win\x64
 pushd build\win\x64
+call :create_ancillary_files
 rem
 cmake -G "Visual Studio 12 Win64" ..\..\..
 msbuild superpawn.sln /p:Configuration=Debug
 if errorlevel 1 goto :fail
 msbuild superpawn.sln /p:Configuration=Release
 if errorlevel 1 goto :fail
-call :create_ancillary_files
 copy release\Superpawn.exe release\Superpawn-%BUILD_NUMBER%-x64.exe
+copy %BUILDINFO% release /y
+copy %CHANGELOG% release /y
 cd release
 ..\..\..\..\%ZIP_EXE% ..\superpawn-windows-x64.zip *.* -x superpawn.exe
 popd
@@ -74,15 +80,13 @@ call gauntlet-test.bat --BUILD_ID %BUILD_ID% --BUILD_NUMBER %BUILD_NUMBER% --BUI
 goto done
 
 :create_ancillary_files
-git log -n 50 > release\ChangeLog.txt
+git log -n 50 > ChangeLog.txt
 if exist %BUILDINFO% del %BUILDINFO%
-echo This information uniquely identifies this build of Superpawn. >> %BUILDINFO%
+echo This information uniquely identifies the current build of Superpawn. >> %BUILDINFO%
 echo Please reference this information when reporting bugs or other issues. >> %BUILDINFO%
-echo --- >> %BUILDINFO%
-echo Build number: %BUILD_NUMBER% >> %BUILDINFO%
-echo Build branch: %BUILD_BRANCH% >> %BUILDINFO%
-echo Build ID:     %BUILD_ID% >> %BUILDINFO%
-echo Build tag:    %BUILD_TAG% >> %BUILDINFO%
+echo Current build number: %BUILD_NUMBER% >> %BUILDINFO%
+echo Current build ID: %BUILD_ID% >> %BUILDINFO%
+echo Current build tag: %BUILD_TAG% >> %BUILDINFO%
 exit /b
 
 :fail
